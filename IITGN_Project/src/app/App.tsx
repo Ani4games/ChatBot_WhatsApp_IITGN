614628d509d07a99a84bb3c690bb1af7af0c3562
= <=>
2026-02-08 11:48:12 +0530

    Added src folder

---
+++ IITGN_Project/src/app/App.tsx
@@ -0,0 +1,9 @@
+import { WhatsAppChat } from "./components/whatsapp-chat";
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-[#111b21] flex justify-center items-center">
+      <WhatsAppChat />
+    </div>
+  );
+}
