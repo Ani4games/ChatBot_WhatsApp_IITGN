5dc1b15bf0e278fe77f11c28d903bd545de730e5
= <=>
2026-02-11 11:52:08 +0530

    Added some more UI

--- IITGN_Project/src/app/App.tsx
+++ IITGN_Project/src/app/App.tsx
@@ -1,9 +1,22 @@
+// import LoginScreen from "../../MobileApp_UI";
 import { WhatsAppChat } from "./components/whatsapp-chat";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "../../protectRoute";
 export default function App() {
   return (
-    <div className="h-screen w-screen bg-[#111b21] flex justify-center items-center">
-      <WhatsAppChat />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/login" element={<LoginScreen />} /> */}
+         <Route
+          path="/whatsapp"
+          element={
+            <ProtectedRoute>
+              <WhatsAppChat />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
